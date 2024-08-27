1c92be9cb83a92e92bc1439f8768a776fb90645a
Matthijs Berends <m.s.berends@umcg.nl>
2024-08-27 08:25:42 +0200

    export plot2

---
+++ R/reexports.R
@@ -0,0 +1,26 @@
+# ===================================================================== #
+#  An R package by Certe:                                               #
+#  https://github.com/certe-medical-epidemiology                        #
+#                                                                       #
+#  Licensed as GPL-v2.0.                                                #
+#                                                                       #
+#  Developed at non-profit organisation Certe Medical Diagnostics &     #
+#  Advice, department of Medical Epidemiology.                          #
+#                                                                       #
+#  This R package is free software; you can freely use and distribute   #
+#  it for both personal and commercial purposes under the terms of the  #
+#  GNU General Public License version 2.0 (GNU GPL-2), as published by  #
+#  the Free Software Foundation.                                        #
+#                                                                       #
+#  We created this package for both routine data analysis and academic  #
+#  research and it was publicly released in the hope that it will be    #
+#  useful, but it comes WITHOUT ANY WARRANTY OR LIABILITY.              #
+# ===================================================================== #
+
+#' @importFrom plot2 plot2
+#' @export
+plot2::plot2
+
+#' @importFrom plot2 get_plot_title
+#' @export
+plot2::get_plot_title
