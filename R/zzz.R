4d9bd3ca186a105568342be8bd25d236e1560c24
Matthijs Berends <m.berends@certe.nl>
2025-10-02 09:57:48 +0100

    fix setting plot2 options

--- R/zzz.R
+++ R/zzz.R
@@ -17,9 +17,12 @@
 #  useful, but it comes WITHOUT ANY WARRANTY OR LIABILITY.              #
 # ===================================================================== #
 
+certeplot2_env <- new.env(parent = emptyenv())
+
 #' @importFrom plot2 register_colour
 #' @importFrom certestyle certe.colours
 .onLoad <- function(libname, pkgname) {
+  # register Certe colours
   suppressMessages(
     register_colour(certe.colours)
   )
@@ -35,16 +38,28 @@
                     certe_sir = c(S = "certegroen", SI = "certegroen", SDD = "certegeel", I = "certegeel", IR = "certeroze", R = "certeroze", N = "grey50"),
                     certe_sir2 = c(S = "certegroen2", SI = "certegroen2", SDD = "certegeel2", I = "certegeel2", IR = "certeroze2", R = "certeroze2", N = "grey50"))
   )
-  
-  options(plot2.colour = "certe",
-          plot2.colour_font_secondary = "certeblauw")
+  # set Certe options for plot2
+  certeplot2_env$options_plot2 <- list(
+    plot2.colour                = "certe",
+    plot2.colour_font_primary   = "black",
+    plot2.colour_font_secondary = "certeblauw",
+    plot2.colour_background     = "white",
+    plot2.colour_sf             = "grey50",
+    plot2.colour_sf_fill        = "certeroze",
+    plot2.font                  = "Source Sans Pro"
+  )
+  options(certeplot2_env$options_plot2)
 }
 
 .onUnload <- function(libpath) {
-  if (identical(getOption("plot2.colour"), "certe")) {
-    options(plot2.colour = NULL)
+  # Only unset the options that were set by the package
+  opts_to_unset <- character()
+  for (opt in names(certeplot2_env$options_plot2)) {
+    if (identical(getOption(opt), certeplot2_env$options_plot2[[opt]])) {
+      opts_to_unset <- c(opts_to_unset, opt)
+    }
   }
-  if (identical(getOption("plot2.colour_font_secondary"), "certeblauw")) {
-    options(plot2.colour_font_secondary = NULL)
+  if (length(opts_to_unset) > 0) {
+    options(setNames(rep(list(NULL), length(opts_to_unset)), opts_to_unset))
   }
 }
